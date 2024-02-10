ddd830fd061831cd1b59fb559bf03603b598e2b3
simantunes008 <simaopfantunes15@duck.com>
2024-02-10 16:59:11 +0000

    TPC1

--- TP1/emd.py
+++ TP1/emd.py
@@ -8,11 +8,12 @@ modalidades = []
 aptos = 0
 imptos = 0
 numero_de_atletas = len(linhas)
+escaloes = {}
 
 for linha in linhas:
     campo = linha.split(",")
-    modalidade = campo[8]
-    apto = campo[12].strip()
+    modalidade = campo[8].strip().lower()
+    apto = campo[12].strip().lower()
     idade = int(campo[5])
     
     # Verifica se a modalidade já está na lista
@@ -26,10 +27,19 @@ for linha in linhas:
     elif apto == 'false':
         imptos += 1
     
-    print(idade)
+    # Calcula o escalão com base na idade
+    escalao = idade // 5 # Divisão inteira por 5
+    if escalao in escaloes:
+        escaloes[escalao] += 1
+    else:
+        escaloes[escalao] = 1
 
 modalidades.sort()
 
 print("Modalidades:", modalidades)
 print("Porcentagem de atletas aptos: {:.2f}%".format(aptos / numero_de_atletas * 100))
 print("Porcentagem de atletas imptos: {:.2f}%".format(imptos / numero_de_atletas * 100))
+print("Distribuição de atletas por escalão etário:")
+for escalao, num_atletas in escaloes.items():
+    porcentagem = (num_atletas / numero_de_atletas) * 100
+    print(f"Escalão {escalao}: {num_atletas} atletas ({porcentagem:.2f}%)")
